a172580fc36659f865b69ec72d0c25f957f0b6fe
planexia <planexia.sa@gmail.com>
2024-08-12 22:31:09 -0300

    docker333

---
+++ src/main/java/com/example/demo/PlanexiaenSpring33Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanexiaenSpring33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanexiaenSpring33Application.class, args);
+	}
+
+}
